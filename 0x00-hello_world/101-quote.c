21678bcbf8abffc42de2d588396a3a92d01b1b4c
B Fonteh <kiehfonteh@gmail.com>
2022-09-08 15:48:25 -0700

    Updated file 100 and 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,8 @@
 #include<unistd.h>
 /**
- *  * main - prints the string followed by a new line to the standrad error
- *   *
- *    * Return: returns 1
- *     */
+ *  main - prints the string followed by a new line to the standrad error
+ *  Return: returns 1
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
