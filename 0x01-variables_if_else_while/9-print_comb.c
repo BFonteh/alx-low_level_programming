2651e6c86556a49b5264027561dd21e41532c5ee
B Fonteh <kiehfonteh@gmail.com>
2022-09-09 15:24:47 -0700

    Updated files

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /* Include the standard input output library header file */
+
 /**
-* This function prints the possible combinations of numbers from 0 to 9
-* Return: the function returns 0 when all is well
-*/
+ * This function prints the possible combinations of numbers from 0 to 9
+ * Return: the function returns 0 when all is well
+ */
 int main(void)
 {
 	int c = 0;
