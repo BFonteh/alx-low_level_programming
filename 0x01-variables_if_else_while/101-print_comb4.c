a705d73f3629f027297ff1db6cdafc23f423cc1f
B Fonteh <kiehfonteh@gmail.com>
2022-09-09 16:06:21 -0700

    Updated documentation

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  Function prints combination of numbers
+ * Function prints combination of numbers
  * 
- *  Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 
