46f0a77b89e88832bd8abcf03f4b94445875c18c
B Fonteh <kiehfonteh@gmail.com>
2022-09-09 10:06:37 -0700

    All is up to date

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	int c = 0;
-	for (; c <= 8 ; c++)
+	for (; c <= 9 ; c++)
 	{
 		putchar(c+'0');
+		if (c==9)
+			break;
 		putchar(',');
 		putchar(' ');
 
 	}
-	putchar(9+'0');
 	putchar('\n');
 }
