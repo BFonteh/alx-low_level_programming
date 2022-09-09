e262956be0ea7171251ab126659728e47e1f78df
B Fonteh <kiehfonteh@gmail.com>
2022-09-09 16:18:40 -0700

    Updated 101 file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,18 +14,18 @@ int k;
 
 for (i = 0 ; i <= 7 ; i++)
 {
-for (j = i+1 ; j <= 8 ; j++)
+for (j = i + 1 ; j <= 8 ; j++)
 {
-for (k = j+1 ; k <= 9 ; k++)
+for (k = j + 1 ; k <= 9 ; k++)
 {
 
 
 
-putchar(i+'0');
-putchar(j+'0');
-putchar(k+'0');
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
 
-if ((i==7) && (j==8) && (k==9))
+if ((i == 7) && (j == 8) && (k == 9))
 {
 break;
 }
@@ -36,7 +36,7 @@ putchar(' ');
 }
 
 }
-	
+
 }
 
 putchar('\n');
