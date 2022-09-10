1c25b99a13200d35b4100ec294c033bf37df2ae1
B Fonteh <kiehfonteh@gmail.com>
2022-09-09 17:51:53 -0700

    Updated 6 file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-/* Include the standard inpu and output library */
 /**
-* This functions prints the numbers from 0 through 9 using puctchar() only
-* The function returns 0 if evrything works well
+* main - this functions prints the numbers from 0
+* through 9 using puctchar() only
+* Return: the function returns 0 if evrything works well
 */
 int main(void)
 {
 	int c = 0;
+
 	for (; c <= 9 ; c++)
-		putchar(c+'0');
+		putchar(c + '0');
 	putchar('\n');
 	return (0);
 }
