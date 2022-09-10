a72b204388ae376a079957ef63a908e36af04832
B Fonteh <kiehfonteh@gmail.com>
2022-09-09 17:57:24 -0700

    8 file update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/* Include the stndard input output library */
 /**
-*  This function prints numbers in base 16 with letters in lowercase
+*  main - this function prints numbers in
+*  base 16 with letters in lowercase
 *  Return: the return value is 0 if everything works fine
 */
 int main(void)
 {
 	char c;
+
 	for (c = '0' ; c <= '9' ; c++)
 		putchar(c);
 	for (c = 'a' ; c <= 'f' ; c++)
