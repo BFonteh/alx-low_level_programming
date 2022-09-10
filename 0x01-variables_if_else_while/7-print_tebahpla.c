883145ab0a741fa597fc1c269359e8951246f3c0
B Fonteh <kiehfonteh@gmail.com>
2022-09-09 17:54:25 -0700

    Updated file 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/* Include the standardr input ouput library */
 /**
-*  This function prints the alphabet in reverse order
+*  main - this function prints the alphabet in reverse order
 *  Return: The values returned is 0 is all works well
 */
 int main(void)
 {
 	char c;
+
 	for (c = 'z' ; c >= 'a' ; c--)
 		putchar(c);
 	putchar('\n');
