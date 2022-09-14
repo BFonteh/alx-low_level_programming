618a6a46f3421b044e48e0f70fd7cc01db7b49f1
B Fonteh <kiehfonteh@gmail.com>
2022-09-13 18:35:22 -0700

    Modified 7 file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,11 +13,7 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
-	if (n < 0)
-		lastDigit = -1 * (n % 10);
-	else
-		lastDigit = n % 10;
-
+	lastDigit = _abs(n % 10);
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
