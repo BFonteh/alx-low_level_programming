302b4802d8a3166f7175f3e837650fee1add9001
B Fonteh <kiehfonteh@gmail.com>
2022-09-19 15:28:19 -0700

    Added new 101 file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,52 +1,74 @@
 #include <stdio.h>
+
+
+
+
 #include <stdlib.h>
+
+
 #include <time.h>
 
-/**
- * main - generates random passwords for 101-crackme
- * Return: zero
- */
-int main(void)
-{
-	int sum;
-	char c;
 
-	srand(time(NULL));
-	while (sum <= 2645)
-	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
-	return (0);
-}
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-cat > 101-keygen.c
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
+
+
 
 /**
- * main - generates random passwords for 101-crackme
- * Return: zero
- */
+
+
+* main - generates random passwords for 101-crackme
+
+
+* Return: zero
+
+
+*/
+
+
 int main(void)
+
+
 {
-	int sum;
+
+
+	int sum=0;
+
+
 	char c;
 
+
+
+
+
+
+
 	srand(time(NULL));
+
+
 	while (sum <= 2645)
+
+
 	{
+
+
 		c = rand() % 128;
+
+
 		sum += c;
+
+
 		putchar(c);
+
+
 	}
+
+
 	putchar(2772 - sum);
+
+
 	return (0);
+
+
 }
+
