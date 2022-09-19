95e5e0758ce1e6375c3669cec11c996c0616e51b
B Fonteh <kiehfonteh@gmail.com>
2022-09-19 13:24:33 -0700

    Added 0 file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_99.c
@@ -0,0 +1,11 @@
+/** reset_to_98 - Updates value of pointer to 98
+ * @*n: Pointer to the integer n
+ *
+ * Return: Return 1 when all works well
+ */
+void rest_to_98(int *n)
+{
+	*n = 98;
+	
+	return (0);
+}
