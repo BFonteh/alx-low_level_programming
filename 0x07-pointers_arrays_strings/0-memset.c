5f5dd5025064dee942a142ff84768188a0b6d547
B Fonteh <kiehfonteh@gmail.com>
2022-09-26 14:25:13 -0700

    Created 0 file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * *_memset - fills memory with a constant byte.
@@ -8,14 +8,14 @@
  * Return: s
  */
 
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
 
-	for (i = 0; n > 0; i++, n--)
-	{
-		s[i] = b;
-	}
+for (unsigned int i = 0; n > 0 ; i++, n--)
+{
+	s[i] = b;
+}
 
-	return (s);
+return (s);
 }
